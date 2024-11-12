eec2946a4e187456221b2dd061b5676c46b86bd6
MAAD03 <mescobarc20@miumg.edu.gt>
2024-11-12 00:22:18 -0600

    Inicio de proyecto
    Creacion de back end, front end, y carpetas de proyectos

---
+++ Proyecto Recuperacion Back/main/src/test/java/com/bd1_2024/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd1_2024.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
