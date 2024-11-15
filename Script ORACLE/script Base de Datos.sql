5bc8cdb755653553519e259a6822be6a5da6db85
MAAD03 <mescobarc20@miumg.edu.gt>
2024-11-14 19:10:06 -0600

    avances
    Avances de estructura basandome el modelo fisico

---
+++ Script ORACLE/script Base de Datos.sql
@@ -0,0 +1,45 @@
+--menu
+CREATE SEQUENCE menu_seq START WITH 1 INCREMENT BY 1;
+CREATE TABLE MENU(
+id_menu NUMBER PRIMARY KEY,
+nombre_menu VARCHAR2(100),
+ruta VARCHAR2(100),
+fecha_modif DATE,
+usuario_modif VARCHAR2(100)
+);
+
+--rol
+
+
+--menu_rol
+
+
+--usuario
+
+
+--bitacora
+
+
+--marca
+
+
+--motocicleta
+
+
+--sucursal
+
+
+--tipo estado motocicleta
+
+
+--inventario
+
+
+--metodo renta
+
+
+--tipo estado reservacion
+
+
+--reservacion
+
